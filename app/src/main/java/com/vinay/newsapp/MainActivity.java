fbe61e773a9e9dee7027bb3e3a963a328da20caa
vinay kumar <vk1230460@gmail.com>
2025-06-07 15:13:09 +0530

    first commit

--- app/src/main/java/com/vinay/newsapp/MainActivity.java
+++ app/src/main/java/com/vinay/newsapp/MainActivity.java
@@ -19,49 +19,10 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "API_Response";
     private static final String URL = "https://newsdata.io/api/1/latest?apikey=pub_5230629002244320a8ede36ac1fea37c&q=alwar";
-    ListView listView;
     ArrayList<String> arrayList =new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView =findViewById(R.id.listView);
-        ArrayAdapter<String> adapter =new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,arrayList);
-        listView.setAdapter(adapter);
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                URL,
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            Log.d(TAG, "Response received");
-                            if(response.getString("status").equals("success"))
-                            {
-                                JSONArray jsonArray = response.getJSONArray("results");
-                                for(int i=0;i<jsonArray.length();i++)
-                                {
-                                    JSONObject jsonObject =jsonArray.getJSONObject(i);
-                                    String name = jsonObject.getString("title");
-                                    arrayList.add(name);
-                                    adapter.notifyDataSetChanged();
-                                }
-                            }
-                        } catch (Exception e) {
-                            Log.e(TAG, "Parsing error: " + e.getMessage());
-                        }
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e(TAG, "Volley error: " + error.getMessage());
-                    }
-                });
-
-        requestQueue.add(jsonObjectRequest);
     }
 }
