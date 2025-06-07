e8837dd79a4da49b45815be91479cb724192e0cf
vinay kumar <vk1230460@gmail.com>
2025-06-07 15:14:01 +0530

    first commit

--- app/src/main/java/com/vinay/newsapp/MainActivity.java
+++ app/src/main/java/com/vinay/newsapp/MainActivity.java
@@ -5,17 +5,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "API_Response";
     private static final String URL = "https://newsdata.io/api/1/latest?apikey=pub_5230629002244320a8ede36ac1fea37c&q=alwar";
