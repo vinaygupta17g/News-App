33d340d487684f85e946d8b8c16a47f17c1f74df
vinay kumar <vk1230460@gmail.com>
2025-06-16 10:59:00 +0530

    first commit

--- app/src/main/java/com/vinay/newsapp/DetailActivity.java
+++ app/src/main/java/com/vinay/newsapp/DetailActivity.java
@@ -1,6 +1,8 @@
 package com.vinay.newsapp;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class DetailActivity extends AppCompatActivity {
-
+    ImageView imageView;
+    TextView title,description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
